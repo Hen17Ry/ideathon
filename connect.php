194fffcf6b5064b52d059b8250c917102d69eca0
Hen17Ry <henry.gossou17@gmail.com>
2024-04-05 07:54:45 +0100

    first commit

---
+++ connect.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Css/style.css">
+    <title>Connectez-Vous</title>
+</head>
+<body>
+    <div class="conteneur">
+    <div class="text">
+        <h1>Renseignez vos information pour vous connecter</h1>
+        <h1>Et si vous n'avez pas de compte créer en un</h1>
+    </div>  
+    <div class="container">
+        <div class="login-box">
+            <h2 style="color:black">Login</h2>
+            <form action="#">
+                <div class="input-box">
+                    <input type="email" required>
+                    <label style="color:black">Email</label>
+                </div>
+                <div class="input-box">
+                    <input type="password" required>
+                    <label style="color:black">Password</label>
+                </div>
+                <div class="forgot-pass">
+                    <a href="#" style="color:black">Forgot your password?</a>
+                </div>
+                <button type="submit" class="btn"><a href="indexx.html" style="color: black;">Se Connecter</a></button>
+                <div class="signup-link">
+                    <a href="inscription.php" style="color: black;">S'inscrire</a>
+                </div>
+            </form>
+        </div>
+        <span style="--i:0;"></span>
+        <span style="--i:1;"></span>
+        <span style="--i:2;"></span>
+        <span style="--i:3;"></span>
+        <span style="--i:4;"></span>
+        <span style="--i:5;"></span>
+        <span style="--i:6;"></span>
+        <span style="--i:7;"></span>
+        <span style="--i:8;"></span>
+        <span style="--i:9;"></span>
+        <span style="--i:10;"></span>
+        <span style="--i:11;"></span>
+        <span style="--i:12;"></span>
+        <span style="--i:13;"></span>
+        <span style="--i:14;"></span>
+        <span style="--i:15;"></span>
+        <span style="--i:16;"></span>
+        <span style="--i:17;"></span>
+        <span style="--i:18;"></span>
+        <span style="--i:19;"></span>
+        <span style="--i:20;"></span>
+        <span style="--i:21;"></span>
+        <span style="--i:22;"></span>
+        <span style="--i:23;"></span>
+        <span style="--i:24;"></span>
+        <span style="--i:25;"></span>
+        <span style="--i:26;"></span>
+        <span style="--i:27;"></span>
+        <span style="--i:28;"></span>
+        <span style="--i:29;"></span>
+        <span style="--i:30;"></span>
+        <span style="--i:31;"></span>
+        <span style="--i:32;"></span>
+        <span style="--i:33;"></span>
+        <span style="--i:34;"></span>
+        <span style="--i:35;"></span>
+        <span style="--i:36;"></span>
+        <span style="--i:37;"></span>
+        <span style="--i:38;"></span>
+        <span style="--i:39;"></span>
+        <span style="--i:40;"></span>
+        <span style="--i:41;"></span>
+        <span style="--i:42;"></span>
+        <span style="--i:43;"></span>
+        <span style="--i:44;"></span>
+        <span style="--i:45;"></span>
+        <span style="--i:46;"></span>
+        <span style="--i:47;"></span>
+        <span style="--i:48;"></span>
+        <span style="--i:49;"></span>
+    </div>
+    </div>
+</body>
+</html>
